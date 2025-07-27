4eebfe4adae3b7e2cfc247d199412cbca17ddff9
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2025-07-27 22:04:04 +0500

    Created a socket connection and bind on Server side

--- Server.c
+++ Server.c
@@ -1,17 +1,56 @@
+#include<stdio.h>
+#include<sys/socketvar.h>
+#include<stdlib.h>
+#include<arpa/inet.h>
+
 // IPV4 AF_INET sockets
+//struct in_addr {
+//  unsigned long s_addr;             // load with inet_pton 
+//};
+//
+//struct sockaddr_in {
+//  short           sin_family;       //e.g AF_INET, AF_INET6
+//  unsigned short  sin_port;         //e.g, htons(3490)
+//  struct in_addr  sin_addr;         // see struct in_addr, below
+//  char            sin_zero[8];      // zero this if you want to
+//};
+//
+//struct sock_addr {
+//  unsigned short    sa_family;      // address family, AF_xxxx
+//  char              sa_data[14];     // 14 bytes of protocol address
+//};
 
-struct in_addr {
-  unsigned long s_addr;             // load with inet_pton 
-};
 
-struct sockaddr_in {
-  short           sin_family;       //e.g AF_INET, AF_INET6
-  unsigned short  sin_port;         //e.g, htons(3490)
-  struct in_addr  sin_addr;         // see struct in_addr, below
-  char            sin_zero[8];      // zero this if you want to
-};
+int main(int argc ,char *argv[])
+{
+  int socket_desc;
+  struct sockaddr_in server;
 
-struct sockaddr {
-  unsigned short    sa_family;      // address family, AF_xxxx
-  char              sa_data[14];     // 14 bytes of protocol address
-};
+  socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (socket_desc == EXIT_FAILURE) 
+  {
+    printf("Could not create socket");
+    printf("%d", EXIT_FAILURE);
+  }
+
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = INADDR_ANY;
+  server.sin_port = htons(8888);
+
+  if(bind(socket_desc, (struct sockaddr *)&server , sizeof(server)) < 0 )
+  {
+    puts("bind failed");
+  }
+ 
+  puts("bind done");
+
+  return 0; 
+}
+
+
+
+  //struct sockaddr_in server;
+//server.sin_port = htons(80);
+//server.sin_addr.sin_family = inet_addr("")
+
