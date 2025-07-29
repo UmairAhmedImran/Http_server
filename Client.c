7e1b757f75bb65af36799801679ea30c96a6422f
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2025-07-29 23:46:02 +0500

    getting hostname in the client

--- Client.c
+++ Client.c
@@ -1,9 +1,32 @@
 // Description of database entry of single host 
-struct hostnet 
+//struct hostnet 
+//{
+//  char *h_name;         // Official name of host
+//  char **h_aliases;     // Alias List
+//  int h_addrtype;       // Host address type
+//  int h_length;         // Length of address
+//  char **h_addr_list;   // list of address from name server
+//};
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include<stdlib.h>
+#include <unistd.h> /* close */
+
+
+int main(int argc, char *argv[])
 {
-  char *h_name;         // Official name of host
-  char **h_aliases;     // Alias List
-  int h_addrtype;       // Host address type
-  int h_length;         // Length of address
-  char **h_addr_list;   // list of address from name server
-};
+   struct hostent *h;
+  
+  h = gethostbyname(argv[1]);
+  if(h==NULL) 
+  {
+    printf("%s: unknown host '%s'\n",argv[0],argv[1]);
+    EXIT_FAILURE;
+  }
+
+}
