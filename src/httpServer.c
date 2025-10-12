05789bb972e2118d061f1945cd079c0ba794e8be
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2025-10-12 23:07:50 +0500

    chore(feat): finalizing the http server

--- src/httpServer.c
+++ src/httpServer.c
@@ -12,6 +12,13 @@
 #define BUFFER_SIZE 4096
 #define SERVER_PORT 8000
 
+struct Response {
+    int status_code;
+    char content_type[64];
+    char body[1024];
+};
+
+
 struct Header {
     char key[50];
     char value[256];
@@ -25,6 +32,39 @@ struct Request {
     int header_count;
     char *body;
 };
+
+void send_response(int client_socket, struct Response *res) {
+    char buffer[2048];
+
+    // Prepare status line
+    char *status_text;
+    switch (res->status_code) {
+        case 200: status_text = "OK"; break;
+        case 400: status_text = "Bad Request"; break;
+        case 404: status_text = "Not Found"; break;
+        case 500: status_text = "Internal Server Error"; break;
+        default: status_text = "Unknown";
+    }
+
+    // Format full response
+    int len = snprintf(buffer, sizeof(buffer),
+        "HTTP/1.1 %d %s\r\n"
+        "Content-Type: %s\r\n"
+        "Content-Length: %zu\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        "%s",
+        res->status_code,
+        status_text,
+        res->content_type,
+        strlen(res->body),
+        res->body
+    );
+
+    // Send the response
+    send(client_socket, buffer, len, 0);
+}
+
 
 void parse_http_request(char *recv_buffer, struct Request *req) {
     char *saveptr_outer, *saveptr_inner;
@@ -84,30 +124,13 @@ void parse_http_request(char *recv_buffer, struct Request *req) {
 
 int main(int argc, char *argv[])
 {
-  // Request struct
-  //struct Request request_struct;
-  // taget variables to find out
-  char target_colon = ':';
-  char target_empty_line[] = ""; 
-
-  // booleans to find out the position of client requests
-  bool in_header = true; 
-  bool in_body = false; 
 
   // socket variables
   int server_socket, client_socket, c;
-  int line_no = 0;
   struct sockaddr_in server_addr, client_addr;
-  char *buffer = 
-    "HTTP/1.1 200 OK\r\n"
-    "Content-Type: text/plain\r\n"
-    "Content-Lenght: 18\r\r\n\n"
-    "{message: ok}\n";
   char recv_buffer[BUFFER_SIZE] = {0};
-  ssize_t bytes_send, bytes_recv;
+  ssize_t bytes_recv;
 
-  // save pointer for strtok_r
-  char *saveptr_outer, *saveptr_inner;
 
 
   // 1. creating a socket
@@ -158,12 +181,9 @@ int main(int argc, char *argv[])
     printf("Socket listen successfully\n");
   }
 
-
   while(1) 
   {
-  
   // 5. accepting the client connections
-
   puts("waiting for incoming connections...");
   c = sizeof(struct sockaddr_in);
   // LOL was connecting the accept with client_socket thats why it was running continuously and giving error, finally understood how stupid I am
@@ -173,15 +193,6 @@ int main(int argc, char *argv[])
     perror("accept failed");
     return FAILURE;
   } 
-
-  bytes_send = send(client_socket, buffer, strlen(buffer), 0);
-
-  if (bytes_send == -1)
-  {
-    perror("send failed");
-  } else {
-    printf("Sent %zd bytes to client.\n", bytes_send);
-  }
 
   bytes_recv = recv(client_socket, recv_buffer, BUFFER_SIZE,  0);
 
@@ -201,62 +212,17 @@ int main(int argc, char *argv[])
         printf("%s: %s\n", req.headers[i].key, req.headers[i].value);
     }
     printf("\nBody:\n%s\n", req.body ? req.body : "(none)");
+    struct Response res;
+    res.status_code = 200;
+    strcpy(res.content_type, "application/json");
+    snprintf(res.body, sizeof(res.body),
+         "{\"message\": \"Hello from C HTTP Server\", \"path\": \"%s\"}",
+         req.path);
+
+    send_response(client_socket, &res);
 
     free(req.body);
-    
-    //printf("RECV BUFFER: \n %s", recv_buffer);
-    //printf("\n ----------RECV BUFFER END----------\n");
-    ////int position_of_end_of_headers = strcspn(recv_buffer, "\r\r\n\n"); // usign strtok as of now but should use strtok_r for multithreading later
-    //char *split_by_strtok = strtok_r(recv_buffer, "\n", &saveptr_outer);
-
- // //  char *ptr_client_data_by_line = strtok(recv_buffer, "\n");
-
-    //while (split_by_strtok != NULL)
-    //{
-    //  printf("\nprinting the line\n %s", split_by_strtok);
-    //  split_by_strtok = strtok_r(NULL, "\n", &saveptr_outer);
-    //  if (split_by_strtok != NULL)
-    //  {
-    //    printf("\nprinting the line\n %s", split_by_strtok);
-    //    if (line_no == 0)
-    //    {
-    //      char *split_by_spaces = strtok_r(split_by_strtok, " ", &saveptr_inner);
-    //      while (split_by_spaces != NULL)
-    //        {
-    //          split_by_spaces = strtok_r(NULL, " ", &saveptr_inner);
-    //          if (split_by_spaces != NULL)
-    //          {
-    //            //request_struct
-    //            printf("\n splitting by spaces %s \n", split_by_spaces);
-    //            
-    //          }
-    //        }
-    //      } 
-    //  
-    //  if (in_header && line_no > 1) 
-    //  {
-    //    char *split_by_colon = strtok(split_by_strtok, ":");
-    //    while(split_by_colon != NULL)
-    //    {
-    //      split_by_colon = strtok(NULL, ":");
-    //      if (split_by_colon != NULL)
-    //      {
-    //        printf("\n Splitting by colon: %s\n", split_by_colon);
-    //      }
-    //    }
-    //  }
-    //  
-    //  
-    //  }
-
-    //   line_no += 1;
-    //}
-   //printf("position: %d\n", position_of_end_of_headers);
-    
-   // printf("request line and headers: %s\n", recv_buffer[position_of_end_of_headers]); // completely wrong have to do apply strncpy or putchar for this
   }
-  
-  
 
   close(client_socket);
   //close(server_socket);
